93c2877e01c8bfbcf07eaeabd7398bef91a5cb93
Anthony D'Apruzzo <64275779+dapruzav@users.noreply.github.com>
2024-01-18 16:57:08 -0500

    Delete 3confusion_matrix.R

--- 3confusion_matrix.R
+++
@@ -1,9 +0,0 @@
-table(predicted = y_hat, actual = test_set$sex)
-
-test_set %>%
-  mutate(y_hat = y_hat) %>%
-  group_by(sex) %>%
-  summarize(accuracy = mean(y_hat == sex))
-
-prev <- mean(y == "Female")
-prev
